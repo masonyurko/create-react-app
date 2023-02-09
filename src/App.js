57cf40f00b4c5309fd00dcdae9192b7067c02d0e
masonyurko <122504955+masonyurko@users.noreply.github.com>
2023-02-08 22:31:46 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
 <>
     <div class="outline">
       <div class="lecard">
-        <h2>Some Nerd</h2>
+        <h2>SomNerd</h2>
         <img src="https://i.kym-cdn.com/news_feeds/icons/mobile/000/037/359/05b.jpg" alt="Obi-Wan headshot" />
         <p class="details">Hello there <br></br> Lightsaber go <i>brrrr</i></p>
       </div>
